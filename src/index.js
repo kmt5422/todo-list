a3bcf5fc432d522de48e792b95484c1bdac3dc93
Kevin Thompson <kevinthompson.nmtcs@gmail.com>
2020-06-07 17:28:44 -0400

    added form to create a TodoList

--- src/index.js
+++ src/index.js
@@ -2,21 +2,66 @@ import createTodo from './modules/todo';
 import createTodoList from './modules/todo-list';
 import renderer from './modules/renderer';
 
-// TESTING CODE BELOW
+// DOM Elements
+const contentDiv = document.querySelector('.container');
+let createTodoListForm;
+let pageHeadingDiv;
+let todoListsDiv;
+const todoLists = [];
 
-let todo = createTodo('Test Todo', 'Just a test Todo Object', '05/25/2022', 'low');
-let todo2 = createTodo('Test Todo 2', 'Just another Todo Object', '05/26/2022', 'medium');
-let todo3 = createTodo('Todo 3', 'Nothing new', '12/23/2020', 'high');
+loadPage();
 
-let todoList = createTodoList('TodoList');
-todoList.addTodo(todo);
-todoList.addTodo(todo2);
-todoList.addTodo(todo3);
+function loadPage() {
+    pageHeadingDiv = document.createElement('div');
 
-console.log(todo);
-console.log(todo.getTitle());
-console.log(todo2);
-console.log(todo2.getTitle());
-console.log(todoList.getTodos());
+    let pageHeading = document.createElement('h1');
+    pageHeading.textContent = 'Todo List Project';
+    pageHeadingDiv.appendChild(pageHeading);
 
-renderer.renderTodoList(todoList);
+    let addTodoListBtn = document.createElement('button');
+    addTodoListBtn.textContent = 'Create New Todo List';
+    pageHeadingDiv.appendChild(addTodoListBtn);
+
+    // Event Listeners
+    addTodoListBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        // Bring up form to create new Todo List
+        contentDiv.removeChild(pageHeadingDiv);
+        showCreateTodoListForm();
+    });
+
+    contentDiv.appendChild(pageHeadingDiv);
+}
+
+function showCreateTodoListForm() {
+    if (!createTodoListForm) {
+        // Create the form
+        createTodoListForm = document.createElement('div');
+
+        let fieldLabel = document.createElement('label');
+        let namefield = document.createElement('input');
+        let submitBtn = document.createElement('button');
+
+        fieldLabel.textContent = 'Todo List Name: ';
+        fieldLabel.setAttribute('for', 'name-field');
+        namefield.id = 'name-field';
+        submitBtn.textContent = 'Create Todo List';
+
+        createTodoListForm.appendChild(fieldLabel);
+        createTodoListForm.appendChild(namefield);
+        createTodoListForm.appendChild(submitBtn);
+
+        submitBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            // Add a new Todo List
+
+            contentDiv.removeChild(createTodoListForm);
+            contentDiv.appendChild(pageHeadingDiv)
+        })
+
+        contentDiv.appendChild(createTodoListForm);
+    }
+    createTodoListForm.classList.add('show-form');
+}
